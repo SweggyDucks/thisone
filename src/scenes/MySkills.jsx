ac66313daf9c39b9beb4720ef29cbabd71eaedd5
SweggyDucks <cyrushj@gmail.com>
2023-05-10 22:58:28 -0700

    changes

--- src/scenes/MySkills.jsx
+++ src/scenes/MySkills.jsx
@@ -70,7 +70,7 @@ const MySkills = () => {
             <div className="w-1/2 md:w-1/4 h-32 bg-blue absolute right-0 top-0 z-[-1]" />
           </div>
           <p className="mt-5">
-            I have collaborated with a number of esteemed corporate clientele, including Fortune 50 enterprises, thereby honing my expertise in handling diverse and complex business challenges. Additionally, I am a certified Microsoft Excel professional and have the distinction of being the youngest employee to attain certification as an Oracle Narrative Reporting Implementer.
+            I have collaborated with a number of esteemed corporate clientele, including Fortune 50 enterprises, on improving their capital planning systems. These experiences honed my expertise in handling diverse and complex business challenges. Additionally, I am a certified Microsoft Excel professional and have the distinction of being the youngest employee to attain certification as an Oracle Narrative Reporting Implementer.
           </p>
         </motion.div>
 
@@ -96,7 +96,7 @@ const MySkills = () => {
             <div className="w-1/2 md:w-1/4 h-32 bg-red absolute right-0 top-0 z-[-1]" />
           </div>
           <p className="mt-5">
-          I completed my undergraduate studies at a top 100 business school, earning a Bachelor of Science degree in Finance within a remarkably short span of three years. I previously held a position as a financial educator at a company that recently underwent an IPO on the NASDAQ. In this role, I delivered lectures on a range of topics, including financial modeling, technical analyses, and fundamental analyses, thereby sharpening my communication and instructional abilities.
+          I completed my undergraduate studies at a top 100 business school, earning a Bachelor of Science degree in Finance within a remarkably short span of three years. I previously held a position as a financial educator at a company that recently underwent an IPO on the NASDAQ. In this role, I sharpened my expertise in financial modeling, technical analyses, and fundamental analyses.
           </p>
         </motion.div>
         {/* INTERESTS */}
@@ -121,7 +121,7 @@ const MySkills = () => {
             <div className="w-1/2 md:w-1/4 h-32 bg-green absolute right-0 top-0 z-[-1]" />
           </div>
           <p className="mt-5">
-          As a licensed real estate agent, I have completed 150 hours of extensive training, which has contributed to my ability to successfully close five deals within the first year of being licensed. Moreover, I have demonstrated my leadership and managerial skills by running a real estate investment company with a team of more than 10 employees, thus gaining valuable experience in real estate operations and personnel management.
+          As a licensed real estate agent, I have completed 150 hours of extensive training. Moreover, I have demonstrated my leadership and managerial skills by running a real estate investment company with a team of 11 employees, thus gaining valuable experience in real estate operations and personnel management.
           </p>
         </motion.div>
       </div>
